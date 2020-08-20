40aa5b192fc307b10a2515494aaf4aa3430c057c
vaibhavjhingran <vaibhavjhingran@gmail.com>
2020-08-20 16:01:01 +0200

    Added minor additions to the app

--- app/src/main/java/com/ice/rolldadice/MainActivity.kt
+++ app/src/main/java/com/ice/rolldadice/MainActivity.kt
@@ -3,27 +3,39 @@ package com.ice.rolldadice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollBtn: Button = findViewById(R.id.rollBtn)
+        val rollBtn: Button = findViewById(R.id.roll_btn)
         rollBtn.setOnClickListener{
-            Toast.makeText(this, "Okay, we be rollin'...", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
 
     }
 
     private fun rollDice() {
-        val result: TextView = findViewById(R.id.textView)
-
         val randomInt = Random.nextInt(6) + 1
-        result.text = randomInt.toString()
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
     }
 }
